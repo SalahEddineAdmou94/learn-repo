76f487465c7cd67880d7e3815cfacbb9b9381502
salah-eddine <salma.1994.199921@gmail.com>
2023-03-03 17:28:42 +0100

    working

---
+++ ts.ts
@@ -0,0 +1,9 @@
+const first = useContext(second)
+
+function useContext(second: any) {
+    throw new Error("Function not implemented.");
+}
+function second(second: any) {
+    throw new Error("Function not implemented.");
+}
+
